b8e3b1b38811ae8eb5e708aa311b89933da4fc74
sucheta <sucheta@sucheta-ThinkPad-X230.(none)>
2013-09-05 19:44:56 +0530

    minor fixes in line chart

--- linechart/mapping.js
+++ linechart/mapping.js
@@ -6,34 +6,25 @@ function getData(){
 		console.log(data);
 		var datatolang = [0,0,0,0];
 		var dates = ["Feb 2013", "Jan 2013", "Nov 2012", "Oct 2012"];
-		//datatolang['harsh'] = 'sucheta'; 
-		//datatolang[0] = 'suchu';
 		var flag = 0;
-			$.each(data, function (i,value){
-				//console.log(i);
-				//console.log(value.date);
-				var fullDate = value.date; 
-				var splitDate = fullDate.split(" ");
-				var date = splitDate[1] + " " + splitDate[4] ;
-
-				//datatolang[date] = 1;
-				//console.log(datatolang);
-				//console.log( splitDate[1] + " " + splitDate[4] ); 
-				//console.log(datatolang.length);
-				for (var i = 0 ; i < dates.length ; i++){
-					if(date == dates[i]) {
-						datatolang[i] = datatolang[i] + 1;
-						break;
-					}
- 				}
-
-				
-            });
-
-			var finalDate = [];
+		$.each(data, function (i,value){
+			var fullDate = value.date; 
+			var splitDate = fullDate.split(" ");
+			var date = splitDate[1] + " " + splitDate[4] ;
 			for (var i = 0 ; i < dates.length ; i++){
-					finalDate[dates[i]] = datatolang[i];
-					 				}
-            console.log(finalDate);
+				if(date == dates[i]) {
+					datatolang[i] = datatolang[i] + 1;
+					break;
+				}
+			}
+
+			
+		});
+
+		var finalDate = [];
+		for (var i = 0 ; i < dates.length ; i++){
+			finalDate[dates[i]] = datatolang[i];
+		}
+		console.log(finalDate);
 	})
 }
